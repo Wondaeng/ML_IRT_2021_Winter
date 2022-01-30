b72f92127188a71f3dc1507887bdeeb038c09002
Wondaeng <71166964+Wondaeng@users.noreply.github.com>
2022-01-31 04:29:29 +0900

    Update uncertainty.py

--- Task_8/active_learning/uncertainty.py
+++ Task_8/active_learning/uncertainty.py
@@ -1,28 +1,38 @@
 import cv2
-import math
 from detectron2.engine import DefaultPredictor
 
 
-def uncertainty_score(base_path, pool, cfg, mode='mean_entropy'):
+def uncertainty_score(base_path, pool, cfg, mode='min_confidence'):
     predictor = DefaultPredictor(cfg)
-    entropy_list = []
+    confidence_list = []
     for f in pool:
         image_path = base_path + f
         img = cv2.imread(image_path)
         output = predictor(img)
         num_instances = len(output['instances'])
 
-        if num_instances > 0:
-            entropy_sum = 0
-            for i in range(num_instances):
-                confidence = output['instances'].scores[i].item()
-                score_entropy = (-1) * (confidence) * math.log10(confidence) + (-1) * (1 - confidence) * math.log10(1 - confidence)
-                entropy_sum += score_entropy
-            entropy_mean = entropy_sum/num_instances
-            entropy_list.append((entropy_mean, f))
-        else:
-            entropy_list.append((0, f))
-    return entropy_list
+        if mode == 'min_confidence':  # Criterion: The image contains the least confident instance
+            if num_instances > 0:
+                confidence_score = 1  # 100% confident
+                for i in range(num_instances):
+                    confidence = output['instances'].scores[i].item()
+                    if confidence <= confidence_score:
+                        confidence_score = confidence
+                confidence_list.append((confidence_score, f))
+            else:
+                confidence_list.append((0, f))  # False Negative
 
+        elif mode == 'mean_confidence':
+            if num_instances > 0:
+                confidence_score = 0
+                for i in range(num_instances):
+                    confidence = output['instances'].scores[i].item()
+                    confidence_score += confidence
+                confidence_mean = confidence_score / num_instances
+                confidence_list.append((confidence_mean, f))
+            else:
+                confidence_list.append((1, f))
+
+    return confidence_list
 
 
