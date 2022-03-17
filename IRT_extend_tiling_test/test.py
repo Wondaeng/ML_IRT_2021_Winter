c5cceac11a66e6ddc0598342cfc67a3e259da372
Wondaeng <71166964+Wondaeng@users.noreply.github.com>
2022-03-17 19:06:34 +0900

    To test tiling with example images (n = 4)

--- IRT_extend_tiling_test/test.py
+++ IRT_extend_tiling_test/test.py
@@ -1,10 +1,3 @@
-import json, utils
-
-from collections import OrderedDict
-
-new_json = OrderedDict()
-new_json['images'] = []
-new_json['annotations'] = []
-print(json.dumps(new_json, ensure_ascii=False, indent="\t"))
+import utils
 
 utils.tiling_bbox(4, './test_image/dataset.json', './images_slice' )
